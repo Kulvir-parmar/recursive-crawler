11df0cfe986ad3b55f66f8755637d469b28e02f3
Kulvir Singh <kulvirs734@gmail.com>
2024-05-27 03:18:47 +0530

    fetch URL from HTML

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl";
+
+import { normalizeURL, getURLsFromHTML } from "./crawl";
 
 test("normalizeURL strip protocol", () => {
     const input = "https://blog.boot.dev/path";
@@ -29,6 +30,84 @@ test("normalizeURL strip HTTP", () => {
     const input = "http://blog.boot.dev/path";
     const actual = normalizeURL(input);
     const expected = "blog.boot.dev/path";
+
+    expect(actual).toEqual(expected);
+});
+
+test("get absolute URL from HTML", () => {
+    const inputHTMLBody = `
+                    <html>
+                        <body>
+                            <a href="https://blog.boot.dev/path">
+                                Boot.dev Blog
+                            </a>
+                        </body>
+                    <html>
+                `;
+
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected: string[] = ["https://blog.boot.dev/path"];
+
+    expect(actual).toEqual(expected);
+});
+
+test("get relative URL from HTML", () => {
+    const inputHTMLBody = `
+                    <html>
+                        <body>
+                            <a href="/path/">
+                                Boot.dev Blog
+                            </a>
+                        </body>
+                    <html>
+                `;
+
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected: string[] = ["https://blog.boot.dev/path/"];
+
+    expect(actual).toEqual(expected);
+});
+
+test("get both types of URLs from HTML", () => {
+    const inputHTMLBody = `
+                    <html>
+                        <body>
+                            <a href="https://blog.boot.dev/path1/">
+                                Boot.dev Blog1
+                            </a>
+                            <a href="/path2/">
+                                Boot.dev Blog2
+                            </a>
+                        </body>
+                    <html>
+                `;
+
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected: string[] = [
+        "https://blog.boot.dev/path1/",
+        "https://blog.boot.dev/path2/",
+    ];
+
+    expect(actual).toEqual(expected);
+});
+
+test("get invalid URLs from HTML", () => {
+    const inputHTMLBody = `
+                    <html>
+                        <body>
+                            <a href="invalid">
+                                invalid
+                            </a>
+                        </body>
+                    <html>
+                `;
+
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected: string[] = [];
 
     expect(actual).toEqual(expected);
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,33 @@
+import * as jsdom from "jsdom";
+
+const { JSDOM } = jsdom;
+
+function getURLsFromHTML(htmlBody: string, baseURL: string): string[] {
+    const urls: string[] = [];
+
+    const dom = new JSDOM(htmlBody);
+    const anchorElements = dom.window.document.querySelectorAll("a");
+
+    for (const anchor of anchorElements) {
+        if (anchor.href.slice(0, 1) === "/") {
+            try {
+                const urlObj = new URL(`${baseURL}${anchor.href}`);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log("error with relative url", error);
+            }
+        } else {
+            try {
+                const urlObj = new URL(anchor.href);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log("error with absolute url", error);
+            }
+        }
+    }
+    return urls;
+}
+
 function normalizeURL(urlString: string): string {
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,4 +38,4 @@ function normalizeURL(urlString: string): string {
     return hostPath;
 }
 
-export { normalizeURL };
+export { normalizeURL, getURLsFromHTML };
